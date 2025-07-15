b7bfd8991c62931afebf2a687c64b51b182f5429
Tgoat1710 <trongmaster1710@gmail.com>
2025-07-15 23:53:03 +0700

    sửa lại cho phù hợp mưới function

--- client/src/router/managerRouter.tsx
+++ client/src/router/managerRouter.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import ManagerDashboard from "../pages/ManagerPages/ManagerDashboard";
 import AlertsAndNotifications from "../pages/ManagerPages/AlertsAndNotifications";
@@ -7,7 +6,8 @@ import HealthRecords from "../pages/ManagerPages/HealthRecords";
 import MedicalStaffManagement from "../pages/ManagerPages/MedicalStaffManagement";
 import VaccinationCampaigns from "../pages/ManagerPages/VaccinationCampaigns";
 import CampaignConsents from "../pages/ManagerPages/CampaignConsents";
-import HealthCheckCampaigns from "../pages/ManagerPages/HealthCheckCampaigns"; // <-- Thêm dòng này
+import HealthCheckCampaigns from "../pages/ManagerPages/HealthCheckCampaigns";
+import QuickAccess from "../pages/ManagerPages/QuickAccess";
 
 const ManagerRouter: React.FC<{ onLogout?: () => void }> = ({ onLogout }) => {
   const safeLogout = onLogout || (() => {});
@@ -17,8 +17,9 @@ const ManagerRouter: React.FC<{ onLogout?: () => void }> = ({ onLogout }) => {
       <Route path="alerts-and-notifications" element={<AlertsAndNotifications onLogout={safeLogout} />} />
       <Route path="event-and-appointment-management" element={<EventAndAppointmentManagement onLogout={safeLogout} />} />
       <Route path="health-records" element={<HealthRecords onLogout={safeLogout} />} />
+      <Route path="health-records/quick-access" element={<QuickAccess />} />
       <Route path="medical-staff-management" element={<MedicalStaffManagement onLogout={safeLogout} />} />
-      <Route path="health-check-campaigns" element={<HealthCheckCampaigns />} /> {/* Thêm dòng này */}
+      <Route path="health-check-campaigns" element={<HealthCheckCampaigns />} />
       <Route path="vaccination-campaigns" element={<VaccinationCampaigns />} />
       <Route path="vaccination-campaigns/:campaignId/consents" element={<CampaignConsents />} />
       <Route index element={<Navigate to="dashboard" replace />} />
